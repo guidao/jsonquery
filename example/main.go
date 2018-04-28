86b3ac0b2d7703bd9d4671c79daad1e33c0953a8
wangfeng <wangfeng01@qiniu.com>
2018-04-28 12:04:51 +0800

    chagne example

--- example/main.go
+++ example/main.go
@@ -2,32 +2,32 @@ package main
 
 import (
 	"fmt"
-	"github.com/guidao/jsonquery"
+	jq "github.com/guidao/jsonquery"
 )
 
 func main() {
 	//error
-	v := jsonquery.NewLens().Key("inner").GetWithJson(`{}`)
+	v := jq.NewLens().Key("inner").GetWithJson(`{}`)
 	fmt.Println(v.StringOr("default"), v.Error())
 
 	//object
-	hello := jsonquery.NewLens().Key("hello").GetWithJson(`{"hello": "world"}`).StringOr("")
+	hello := jq.NewLens().Key("hello").GetWithJson(`{"hello": "world"}`).StringOr("")
 	fmt.Println(hello) // world
 
 	//array
-	f23, err := jsonquery.NewLens().Key("array").Index(2).GetWithJson(`{"array":["hello", "world", 23]}`).Float64()
+	f23, err := jq.NewLens().Key("array").Index(2).GetWithJson(`{"array":["hello", "world", 23]}`).Float64()
 	fmt.Println(f23, err) //23, nil
 
 	//foreach
-	err = jsonquery.NewLens().Key("array").GetWithJson(`{"array":["hello", "world", 23]}`).ForeachArray(func(i int, v jsonquery.Value) {
+	err = jq.NewLens().Key("array").GetWithJson(`{"array":["hello", "world", 23]}`).ForeachArray(func(i int, v jq.Value) {
 		fmt.Printf("i:%v, v:%v\n", i, v.InterfaceOr(nil))
 		// i:0, v:hello
 		// i:1, v:world
 		// i:2, v:23
 	})
 
-	o := jsonquery.NewLens().Key("array").GetWithJson(`{"array": ["hello", "world"]}`)
-	o = o.Set(jsonquery.NewLens().Index(0), "HELLO")
+	o := jq.NewLens().Key("array").GetWithJson(`{"array": ["hello", "world"]}`)
+	o = o.Set(jq.NewLens().Index(0), "HELLO")
 	fmt.Println(o.InterfaceOr(nil)) //["HELLO", "world"]
 	fmt.Println(err)                //nil
 }
